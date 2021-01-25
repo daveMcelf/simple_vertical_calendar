b842c5c2585843922375672d71221464d42e7bc2
Poleak Chanrith <poleakchanrith@gmail.com>
2021-01-26 03:02:23 +0900

    add package description

--- lib/vertical_calendar.dart
+++ lib/vertical_calendar.dart
@@ -1,8 +1,5 @@
 library vertical_calendar;
 
-// import 'package:flutter/material.dart';
-// import 'package:intl/intl.dart';
-
 export 'src/customization/dayofweek_style.dart';
 export 'src/customization/header_style.dart';
 export 'src/customization/day_style.dart';
